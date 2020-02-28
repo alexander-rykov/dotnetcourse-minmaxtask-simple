a9fb5119f8190aefb4453aa7255eaff26649c4d8
Aliaksandr Rykau <alexander.rykov@gmail.com>
2020-02-28 09:31:05 +0000

    Move assert to main.cs.

--- main.cs
+++ main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 class MainClass {
   public static void Main (string[] args) {
@@ -11,5 +12,24 @@ class MainClass {
     Assert.DoesNotThrow(minTaskTests.Min_XEqualsY_ReturnX);
     Assert.DoesNotThrow(minTaskTests.Min_XGreaterThanY_ReturnY);
     Assert.DoesNotThrow(minTaskTests.Min_XLessThanY_ReturnX);
+  }
+}
+
+
+public static class Assert {
+  public static void AreEqual(int expected, int actual) {
+    if (expected != actual) {
+      throw new Exception($"Actual and expected values are not equal. Expected value: {expected}. Actual value: {actual}.");
+    }
+  }
+
+  public static void DoesNotThrow(Action action) {
+    try {
+      action();
+    }
+    catch (Exception e) {
+      var methodInfo = (action as Delegate).GetMethodInfo();
+      Console.WriteLine($"Test {methodInfo.Name} failed: {e.Message}");
+    }
   }
 }
